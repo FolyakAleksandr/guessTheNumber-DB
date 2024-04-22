2bad013e51dbc14b611213428602dbcdc5ce8c14
FolyakAleksandr <a.folyak2000@gmail.com>
2024-04-22 12:32:25 +0600

    initial setup

---
+++ miniApp-UserDefaults/Extensions/ViewController+Extension.swift
@@ -0,0 +1,5 @@
+extension ViewController {
+    func setupBackground() {
+        view.backgroundColor = .white
+    }
+}
---
+++ miniApp-UserDefaults/Views/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ViewController: UIViewController {
+    // MARK: - Private properties
+    
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackground()
+    }
+    
+    // MARK: - Private methods
+}
